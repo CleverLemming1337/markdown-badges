c2a49742fe4db609459004621757d15ed514b63a
CleverLemming1337 <119337770+CleverLemming1337@users.noreply.github.com>
2024-06-05 19:47:12 +0200

    categories and generated md code

---
+++ website/data.js
@@ -0,0 +1,25 @@
+// list of all MD badges
+const badges = [
+    {
+        "name": "GitHub",
+        "logo": "github",
+        "color": "000000",
+        "text": "ffffff",
+        "category": 0
+    },
+    {
+        "name":"GitHub copilot",
+        "logo":"github-copilot",
+        "color":"0000ff",
+        "text":"000000",
+        "category":0
+    }
+];
+
+// list of all categories
+const categories = [
+    {
+        "name": "Tools",
+        "shortName": "tools" // used as element ids in DOM
+    }
+]
--- website/script.js
+++ website/script.js
@@ -1,22 +1,44 @@
-const badges = [
-    {
-        "name": "GitHub",
-        "logo": "github",
-        "color": "000000",
-        "text": "ffffff"
-    }
-];
-document.addEventListener("DOMContentLoaded", function () {
+// MD code for user to copy
+let mdCode = {}
 
+// execute code when DOMContent was loaded
+document.addEventListener("DOMContentLoaded", main);
 
+// main code
+function main() {
+    // add category sections to html
+    categories.forEach((item, index) => {
+        let category = document.createElement("div")
+
+        // add header
+        let header = document.createElement("h2")
+        header.innerText = item.name
+        category.appendChild(header)
+
+        // add section for badges
+        let badgeSection = document.createElement("div")
+        badgeSection.id = `badges_${item.shortName}`
+        badgeSection.classList.add("section")
+        category.appendChild(badgeSection)
+
+        // add whole section
+        document.getElementById("badges").appendChild(category)
+    })
+
+    // add badges to categories
     badges.forEach((item, index) => {
         let badge = document.createElement("img");
         badge.className = "badge"
         badge.id = index
-        badge.addEventListener("click", function() {
-            console.log(`added ${badges[index].name}`)
-        })
         badge.src = `https://img.shields.io/badge/${item.name}-${item.color}.svg?style=for-the-badge&logo=${item.logo}&logoColor=white`;
-        document.getElementById("badges").appendChild(badge);
+        
+        // function for adding badge on click
+        badge.addEventListener("click", function () {
+            console.log(`added ${item.name}`)
+            mdCode[index] = item.name
+            document.getElementById("md-code").innerText = Object.values(mdCode).join("")
+        })
+
+        document.getElementById(`badges_${categories[item.category].shortName}`).appendChild(badge);
     })
-});
+}
